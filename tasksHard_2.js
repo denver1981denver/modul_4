2552fc2ad4c63c0b3da5a587b6dbb7a0c2af30f8
denver1981denver <dennik1981@gmail.com>
2023-05-12 21:28:14 +0300

    fix

--- tasksHard_2.js
+++ tasksHard_2.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const minimumTaxRate = (15000 / 100) * 13,
+  averageTaxRate = ((50000 - 15000) / 100) * 20 + minimumTaxRate,
+  userIncome = prompt("Введите сумму дохода");
+
+let income = userIncome,
+  percent = 13,
+  calculationFormula = (income / 100) * percent,
+  taxAmount;
+
+if (userIncome <= 15000) {
+
+  taxAmount = calculationFormula;
+
+} else if (userIncome > 50000) {
+  income = userIncome - 50000;
+  percent = 30;
+  taxAmount = calculationFormula + averageTaxRate;
+} else {
+  income = userIncome - 15000;
+  percent = 20;
+  taxAmount = calculationFormula + minimumTaxRate;
+}
+
+console.log('Сумма налога составляет:', taxAmount, 'rub'.toUpperCase());
+
+
+console.log(minimumTaxRate);
+console.log(averageTaxRate);

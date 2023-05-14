47e21c1afdd22d955ab3a28cb2ff4747ec04f5ca
Denis <dennik1981@Gmail.com>
2023-05-15 02:36:56 +0300

    tasks solved

--- tasksHard_2.js
+++ tasksHard_2.js
@@ -1,30 +1,38 @@
 'use strict';
 
+// Во многих странах иная прогрессивная шкала налогообложения и 
+// вычисляется она более сложным способом: 
+
+// Налоговая ставка 13% на доход до 15 000 ₽
+
+// Налоговая ставка 20% на доход выше 15 000 ₽ означает, что 20% уплачивается не со всей суммы, а лишь с той части, которая превосходит 15 000 ₽, но не выше 50 000 ₽
+
+// Налоговая ставка 30% на доход выше 50 000 ₽ означает, что 30% уплачивается не со всей суммы, а лишь с той части, которая превосходит 50 000 ₽.
+
+// Напишите программу, которая корректно сможет посчитать налог и вывести сумму налога в консоль.
+
 const minimumTaxRate = (15000 / 100) * 13,
   averageTaxRate = ((50000 - 15000) / 100) * 20 + minimumTaxRate,
-  userIncome = prompt("Введите сумму дохода");
+  $userIncome = prompt("Введите сумму дохода");
 
-let income = userIncome,
+let income = $userIncome,
   percent = 13,
-  calculationFormula = (income / 100) * percent,
   taxAmount;
 
-if (userIncome <= 15000) {
+if ($userIncome <= 15000) {
+  income = $userIncome ;
+  taxAmount = (income / 100) * percent;
 
-  taxAmount = calculationFormula;
-
-} else if (userIncome > 50000) {
-  income = userIncome - 50000;
+} else if ($userIncome > 50000) {
+  income = $userIncome - 50000;
   percent = 30;
-  taxAmount = calculationFormula + averageTaxRate;
+  taxAmount = (income / 100) * percent + averageTaxRate;
+
 } else {
-  income = userIncome - 15000;
+  income = $userIncome - 15000;
   percent = 20;
-  taxAmount = calculationFormula + minimumTaxRate;
+  taxAmount = (income / 100) * percent + minimumTaxRate;
 }
 
 console.log('Сумма налога составляет:', taxAmount, 'rub'.toUpperCase());
 
-
-console.log(minimumTaxRate);
-console.log(averageTaxRate);

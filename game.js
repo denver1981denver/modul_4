00c81cd35f5854b584b3366340649c85ce0b417c
Denis <dennik1981@Gmail.com>
2023-06-30 19:34:44 +0300

    the final fix

--- game.js
+++ game.js
@@ -1,82 +1,79 @@
 'use strict';
 
- (() => {
+(() => {
+  let checkString;
+
   const FIGURES = ['камень', 'ножницы', 'бумага'];
 
-   const getRandomIntInclusive = () => {
-   return Math.floor(Math.random() * 3);
-  };
+  const getRandomIntInclusive = () => Math.floor(Math.random() * 3);
 
-   const game = () => {
-      const result = {
-        player: 0,
-        computer: 0,
-      };
+  const game = () => {
+    const result = {
+      player: 0,
+      computer: 0,
+    };
+    // проверка на null
+    const checkNull = () => {
+      const userData = prompt(`${[...FIGURES]}?`);
 
-  const checkNull = () => {
-    const userData = prompt(`${[...FIGURES]}?`);
-    
-    if(userData === null) {
-      if (confirm(`Вы действительно хотите выйти? `)) {
-        return null;
-      } else {
-         return checkNull();
-      }
-    }
-
-    else {
-      return checkString(userData);
-    }
-  };
-
-  const checkString = (str) => {
-    if (str === "") return checkNull();
-
-    const converter = FIGURES.find(
-      (element) => element.slice(0, str.length) === str.toLowerCase()
-    );
-
-    if (converter) {
-      return converter;
-    } else {
-      return checkNull();
-    }
-  };
-
-     return function start() {
-      const userMove = checkNull();
-       const computerMove = FIGURES[getRandomIntInclusive()];
-       
-      if (userMove === null) {
-          return alert(`компьютер: ${result.computer}\n игрок: ${result.player}`);
-        }; 
-        
-        if (userMove === computerMove) {
-          alert(`Вы: ${userMove} компьютер:\n  ${computerMove}\n Ничья`);
+      if (userData === null) {
+        if (confirm(`Вы действительно хотите выйти? `)) {
+          return null;
         } else {
-          if ((userMove === "камень" && computerMove === "ножницы") ||
-            (userMove === "ножницы" && computerMove === "бумага") ||
-            (userMove === "бумага" && computerMove === "камень")) {
-            result.player++;
-            alert(`Вы: ${userMove}\n компьютер: ${computerMove}\n Вы выиграли`);
-            if(!confirm('Ещё?')) {
-              return alert(`Результат: Компьютер ${result.computer} Игрок ${result.player}`);
-            }
-          } else {
-            result.computer++;
-            alert(
-              `Вы: ${userMove}\n компьютер: ${computerMove}\n Компьютер выиграл`
-            );            
-          }
+          return checkNull();
         }
+      } else {
+        return checkString(userData);
+      }
+    };
+    // конвертация строки в полную длину и проверка вводимых данных
+    checkString = (str) => {
+      if (str === '') return checkNull();
 
-        return start();
-      };
+      const converter = FIGURES.find(
+          (element) => element.slice(0, str.length) === str.toLowerCase(),
+      );
+
+      if (converter) {
+        return converter;
+      } else {
+        return checkNull();
+      }
     };
 
-   window.RPS = game;
- })();
+    return function start() {
+      const userMove = checkNull();
+      const computerMove = FIGURES[getRandomIntInclusive()];
 
-console.log(Math.floor(Math.random() * 3));
+      if (userMove === null) {
+        return alert(`Результат:\n  Компьютер ${result.computer},\n  
+        Игрок ${result.player}`);
+      }
 
- 
+      if (userMove === computerMove) {
+        alert(`Вы: ${userMove}\nКомпьютер: ${computerMove}\n Ничья`);
+      } else {
+        if ((userMove === 'камень' && computerMove === 'ножницы') ||
+            (userMove === 'ножницы' && computerMove === 'бумага') ||
+            (userMove === 'бумага' && computerMove === 'камень')) {
+          result.player++;
+          alert(`Вы: ${userMove}\nКомпьютер: ${computerMove}\n Вы выиграли`);
+          if (!confirm('Ещё?')) {
+            return alert(`Результат:\n  Компьютер ${result.computer},\n  
+            Игрок ${result.player}`);
+          }
+        } else {
+          result.computer++;
+          alert(`Вы: ${userMove}\nКомпьютер: ${computerMove}\n 
+          Компьютер выиграл`);
+        }
+      }
+
+      return start();
+    };
+  };
+
+  window.RPS = game;
+})();
+
+
--- script.js
+++ script.js
@@ -5,10 +5,3 @@ const startGame = window.RPS();
 startGame();
 
 
-// const TEXT_ENG = ['Do you really want to leave?', 'computer', 'player',
-//  'You won', 'the computer won', 'dead heat', 'More?'];
-
-//  const TEXT_RUS = ['Вы уверены что хотите выйти?', 'компьютер', 'игрок',
-//  'Вы победили', 'Компьютер победил', 'ничья', 'Ещё?'];
-
-

9e72edce02a2b475c0f1f10008f6b217148aed77
Denis <dennik1981@Gmail.com>
2023-07-01 18:02:04 +0300

    adding English

--- game.js
+++ game.js
@@ -3,18 +3,23 @@
 (() => {
   let checkString;
 
-  const FIGURES = ['камень', 'ножницы', 'бумага'];
+  const FIGURES_ENG = ['rock', 'scissors', 'paper'];
+  const FIGURES_RUS = ['камень', 'ножницы', 'бумага'];
 
   const getRandomIntInclusive = () => Math.floor(Math.random() * 3);
 
-  const game = () => {
+  const game = (language) => {
     const result = {
       player: 0,
       computer: 0,
     };
+
+    const lang = language === 'EN' || language === 'ENG' ?
+      FIGURES_ENG : FIGURES_RUS;
+
     // проверка на null
     const checkNull = () => {
-      const userData = prompt(`${[...FIGURES]}?`);
+      const userData = prompt(`${[...lang]}?`);
 
       if (userData === null) {
         if (confirm(`Вы действительно хотите выйти? `)) {
@@ -30,7 +35,7 @@
     checkString = (str) => {
       if (str === '') return checkNull();
 
-      const converter = FIGURES.find(
+      const converter = lang.find(
           (element) => element.slice(0, str.length) === str.toLowerCase(),
       );
 
@@ -43,29 +48,26 @@
 
     return function start() {
       const userMove = checkNull();
-      const computerMove = FIGURES[getRandomIntInclusive()];
+      const computerMove = lang[getRandomIntInclusive()];
 
       if (userMove === null) {
-        return alert(`Результат:\n  Компьютер ${result.computer},\n  
-        Игрок ${result.player}`);
+        return alert(`Результат:\n  Компьютер ${result.computer},\n  Игрок ${result.player}`);
       }
 
       if (userMove === computerMove) {
         alert(`Вы: ${userMove}\nКомпьютер: ${computerMove}\n Ничья`);
       } else {
-        if ((userMove === 'камень' && computerMove === 'ножницы') ||
-            (userMove === 'ножницы' && computerMove === 'бумага') ||
-            (userMove === 'бумага' && computerMove === 'камень')) {
+        if ((userMove === lang[0] && computerMove === lang[1]) ||
+            (userMove === lang[1] && computerMove === lang[2]) ||
+            (userMove === lang[2] && computerMove === lang[0])) {
           result.player++;
           alert(`Вы: ${userMove}\nКомпьютер: ${computerMove}\n Вы выиграли`);
           if (!confirm('Ещё?')) {
-            return alert(`Результат:\n  Компьютер ${result.computer},\n  
-            Игрок ${result.player}`);
+            return alert(`Результат:\n  Компьютер ${result.computer},\n  Игрок ${result.player}`);
           }
         } else {
           result.computer++;
-          alert(`Вы: ${userMove}\nКомпьютер: ${computerMove}\n 
-          Компьютер выиграл`);
+          alert(`Вы: ${userMove}\nКомпьютер: ${computerMove}\n Компьютер выиграл`);
         }
       }
 
